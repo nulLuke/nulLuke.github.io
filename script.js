e5621b2a48258c63277f2f48c3fa1db68c873279
Luke <lukesaccento@icloud.com>
2023-03-12 23:46:46 -0700

    a mere attempt at hamburger menu

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    x.className = "navbar";
   }
-}
+} 
